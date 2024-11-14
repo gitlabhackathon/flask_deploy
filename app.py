8ac1cc1b9484ed3622cc886080c41625f73cf898
gitlabhackathon <gitlabhackathon@gmail.com>
2024-11-14 19:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import math
 
-app = Flask(__name__,template_folder='')
+app = Flask(__name__,template_folder='flask_deploy')
 
 # Store participants and expenses
 participants = {}
